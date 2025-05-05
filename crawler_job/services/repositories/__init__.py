846b6f1bfd9d42fa56f6826641cfcfdfa4a79e22
Quinten M <q.m.b@hotmail.com>
2025-05-05 16:32:46 +0200

    remove config repo because itss annoying

--- crawler_job/services/repositories/__init__.py
+++ crawler_job/services/repositories/__init__.py
@@ -1,9 +1,7 @@
 from .houseRepository import HouseRepository
-from .config_repository import WebsiteConfigRepository
 from .json_config_repository import JsonConfigRepository
 
 __all__ = [
     "HouseRepository",
-    "WebsiteConfigRepository",
     "JsonConfigRepository",
 ]
